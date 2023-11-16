5eb7a561b81243ef6542f4ba2275e9c7d82a3020
Fabio <cassisafabio97@gmail.com>
2023-11-16 10:40:24 +0100

    timer questions, no answer means wrong one

--- src/components/CurrentQuestion.jsx
+++ src/components/CurrentQuestion.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { quiz } from "../reducers/quiz";
 import "../styles.css";
@@ -22,38 +22,61 @@ export const CurrentQuestion = () => {
   const [progressPercentage, setProgressPercentage] = useState(
     ((currentQuestionIndex + 1) / totalQuestions) * 100
   );
-  
+  //set a timerState (questions with countdown)
+  const [timeLeft, setTimeLeft] = useState(10); // Set the initial countdown time
+
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
 
   const handleAnswer = (index) => {
-    const correct = question.correctAnswerIndex === index;
-    setSelectedAnswer(index);
+    // Set a default value for unanswered questions (e.g., 0 for the first option)
+    const answeredIndex = index !== null ? index : 0;
+  
+    const correct = question.correctAnswerIndex === answeredIndex;
+    setSelectedAnswer(answeredIndex);
     setIsAnswerCorrect(correct);
     setShowFeedback(true);
-
+  
     dispatch(
       quiz.actions.submitAnswer({
         questionId: question.id,
-        answerIndex: index,
+        answerIndex: answeredIndex,
         isCorrect: correct,
       })
     );
-
+  
     setTimeout(() => {
       dispatch(quiz.actions.goToNextQuestion());
       setShowFeedback(false);
       setSelectedAnswer(null);
       setIsAnswerCorrect(null);
-
+  
       // Update progress percentage for the progressBar
       setProgressPercentage(
-      ((currentQuestionIndex + 2) / totalQuestions) * 100
+        ((currentQuestionIndex + 2) / totalQuestions) * 100
       );
+  
+      // Reset the timer for the next question
+      setTimeLeft(10);
     }, 2000); // 2 seconds delay
   };
+  
+
+  useEffect(() => {
+    let timer;
+    if (timeLeft > 0) {
+      timer = setTimeout(() => {
+        setTimeLeft((prevTime) => prevTime - 1);
+      }, 1000); // Update the timer every second
+    } else {
+      // Time is up, treat it as an incorrect answer
+      handleAnswer(null);
+    }
+  
+    return () => clearTimeout(timer); // Cleanup timer on component unmount or question change
+  }, [timeLeft, handleAnswer]);
 
   const getButtonStyle = (index) => {
     if (selectedAnswer === index) {
@@ -69,9 +92,12 @@ export const CurrentQuestion = () => {
     <div className="new-page">
       <div className="question-box">
         <h2>{question.questionText}</h2>
+        <div className="counter-time-wrapper">
         <p>
           Question {currentQuestionIndex + 1} / {totalQuestions}
         </p>
+        <p>Time left: {timeLeft}s</p>
+        </div>
         {/* Progress Bar */}
         <div className="progress-bar">
           <div
